8314939f4fd511301a799a23b1a1696d810ebe07
Clive D' Costa <clive.dcosta98@gmail.com>
2024-08-01 15:28:58 +0530

    feat: add a lazy option to the Image component
    if lazy is set to false, the image is loaded immediatley.
    if lazy is set to true, the intersection observer api is used to
    determine whether the image is currently visible to the user and will
    load the image only after the image becomes visible to the user

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -1,10 +1,12 @@
-import { ComponentPropsWithoutRef, useState } from "react";
+import { ComponentPropsWithoutRef, useEffect, useState } from "react";
 import ImgLoaderSVG from "./ImgLoaderSVG";
 import styles from "./index.module.css";
+import useIntersectionObserver from "../../hooks/useIntersectionObserver";
 
 interface ImageTypeProps extends ComponentPropsWithoutRef<"img"> {
     src: string;
     alt: string;
+    lazy: boolean;
     width: string;
     height: string;
 }
@@ -12,9 +14,23 @@ interface ImageTypeProps extends ComponentPropsWithoutRef<"img"> {
 function Image({
     width = "100%",
     height = "100%",
+    lazy,
+    src,
     ...restProps
 }: ImageTypeProps) {
     const [imgLoaded, setImgLoaded] = useState(false);
+    const [imgSrc, setImgSrc] = useState(lazy ? undefined : src);
+    const [imgRef, isImgVisible] = useIntersectionObserver<HTMLImageElement>({
+        threshold: 0.2,
+    });
+
+    useEffect(() => {
+        if (!lazy) return;
+
+        if (isImgVisible) {
+            setImgSrc(src);
+        }
+    }, [isImgVisible, lazy, src]);
 
     function handleOnLoad() {
         setImgLoaded(true);
@@ -26,7 +42,9 @@ function Image({
             style={{ width: width, height: height }}
         >
             <img
+                src={imgSrc}
                 className={styles.imgWrapper__img}
+                ref={lazy ? imgRef : undefined}
                 onLoad={handleOnLoad}
                 draggable={false}
                 {...restProps}
