ba31a3e9d07d94819567de4e382007f724627057
Clive D' Costa <clive.dcosta98@gmail.com>
2024-08-03 03:57:13 +0530

    refactor: integrate new image component

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,30 @@
 import Carousel from "./components/Carousel";
+import Image from "./components/Image";
 
 export default function App() {
+    const slides = [
+        { id: 237, desc: "black puppy" },
+        { id: 238, desc: "Black and white cityscape" },
+        { id: 239, desc: "Dandelion being held" },
+    ].map((obj, index) => (
+        <Image
+            key={obj.id}
+            src={`https://picsum.photos/id/${obj.id}/1600/900`}
+            alt={obj.desc}
+            lazy={index === 0 ? false : true}
+            width="initial"
+            height="initial"
+        />
+    ));
+
     return (
         <Carousel
-            ariaLabel="random"
+            ariaLabel="demo"
             isInfinite={true}
-            width="25rem"
-            height="30rem"
+            width="35rem"
+            height="25rem"
         >
-            {[
-                { id: 237, desc: "black puppy" },
-                { id: 238, desc: "Black and white cityscape" },
-                { id: 239, desc: "Dandelion being held" },
-            ].map((obj, index) => (
-                <img
-                    key={index}
-                    style={{
-                        width: "100%",
-                        height: "100%",
-                        objectFit: "cover",
-                    }}
-                    draggable="false"
-                    alt={obj.desc}
-                    src={`https://picsum.photos/id/${obj.id}/500/300`}
-                />
-            ))}
+            {slides}
         </Carousel>
     );
 }
